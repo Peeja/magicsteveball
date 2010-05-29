bdd9a76a9b9d696001639bfa29ca2ae440c23fc4
Peter Jaros <peter.a.jaros@gmail.com>
2010-05-29 19:31:24 -0400

    Add some answers.

--- data/answers.js
+++ data/answers.js
@@ -1,14 +1,26 @@
 Answers = [
   {
     text: "Sorry,\nno",
-    source: "http://www.macrumors.com/2010/04/12/steve-jobs-confirms-lack-of-future-support-for-original-iphone/"
+    source: "http://www.macstories.net/news/steve-jobs-replies-to-mail-apple-is-dropping-support-for-the-iphone-2g/"
   },
   {
     text: "It will\ncome",
-    source: null
+    source: "http://www.macrumors.com/2010/05/10/steve-jobs-says-printing-will-come-for-ipad/"
   },
   {
-    text: "Check your\nfacts",
-    source: null
+    text: "Not a\nchance",
+    source: "http://gizmodo.com/5545711/dear-steve-has-google-leapfrogged-apple"
+  },
+  {
+    text: "You won't\nbe disap-\npointed",
+    source: "http://www.macrumors.com/2010/05/23/steve-jobs-on-wwdc-announcements-you-wont-be-disappointed/"
+  },
+  {
+    text: "No.",
+    source: "http://www.appleinsider.com/articles/10/03/05/reader_steve_jobs_says_no_tethering_between_ipad_and_iphone.html"
+  },
+  {
+    text: "Yep.",
+    source: "http://www.iphoneincanada.ca/iphone-news/universal-inbox-for-mail-coming-to-iphone-os/"
   }
 ];
