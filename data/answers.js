bbc6b0538a7c29e82efa72433d19801d11b2b26b
Peter Jaros <peter.a.jaros@gmail.com>
2010-05-29 11:52:02 -0400

    Randomize answers.

---
+++ data/answers.js
@@ -0,0 +1,14 @@
+Answers = [
+  {
+    text: "Sorry,\nno",
+    source: "http://www.macrumors.com/2010/04/12/steve-jobs-confirms-lack-of-future-support-for-original-iphone/"
+  },
+  {
+    text: "It will\ncome",
+    source: null
+  },
+  {
+    text: "Check your\nfacts",
+    source: null
+  }
+];
